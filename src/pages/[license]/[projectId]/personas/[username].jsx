6ce33b4b1158cfd533698274defde564f8a6e22e
muslax <muslax@gmail.com>
2020-09-04 17:42:50 +0700

    Fix no-code pages

---
+++ src/pages/[license]/[projectId]/personas/[username].jsx
@@ -0,0 +1,9 @@
+import Layout from 'components/layout/aces'
+
+const Page = () => (
+  <Layout title="About Aces" black="About" blue="Aces">
+    <p>TODO...</p>
+  </Layout>
+)
+
+export default Page
